c8ae146a139a2e027176dd2676a4397b06be8cd0
CMDev97 <cristopher.moccia@gmail.com>
2021-09-10 18:22:55 +0200

    fix

--- src/Constant.js
+++ src/Constant.js
@@ -1,9 +1,8 @@
 
 const type = "http://";
-const domain = "localhost";
+const domain = "www.flicodev.it";
 const port = ":8080";
-const war = "/Gestionale_war/api/";
-
+const war = "/gestionale/api/";
 
 export const Constant = {
     urlBase : type+domain+port+war,
